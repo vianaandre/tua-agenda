ddefb1d6e7c6e2d2397e975934dc85079c17b0bb
vianaandre <andredapperviana@gmail.com>
2022-09-27 19:35:06 -0300

    FEAT: Config environment

---
+++ src/components/Title/Title.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import { Title } from './Title';
+
+describe('<Sidebar />', () => {
+  it('', () => {
+    render(<Title title="Test" />);
+  });
+});
